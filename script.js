61ccc08f74af3e67123872cd8e979c07a0f14398
sprabanjan <sprabanjan2005@gmail.com>
2026-07-13 12:35:41 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome Message
+window.onload = function () {
+    console.log("Welcome to KLN College of Engineering Website");
+};
+
+// Button Click
+function exploreCampus() {
+    alert("Welcome to KLN College of Engineering!");
+}
